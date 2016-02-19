7c9436b034cc77519fb5833c3c02c6c318fc8477
gball <gball@ucmerced.edu>
2016-02-19 12:51:18 -0800

    AngularJS Yeoman Scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gballgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gballgithubioApp
+ */
+angular.module('gballgithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gballgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gballgithubioApp
+ */
+angular.module('gballgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
