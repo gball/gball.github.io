80de9e75d3a2c8eecbf7694d893e2f513939921d
gball <gball@ucmerced.edu>
2016-02-27 22:30:55 -0800

    create education view

--- app/scripts/directives/navigation.js
+++ app/scripts/directives/navigation.js
@@ -20,13 +20,16 @@ angular.module('gballgithubioApp')
     function link (scope) {
       angular.element($window).bind("scroll", function() {
         var headerTop = document.getElementById('header-image').offsetTop;
-        var headerBottom = document.getElementById('header-image').offsetHeight - 10;
-        var aboutBottom = document.getElementById('section-about').offsetHeight;
+        var headerBottom = document.getElementById('header-image').offsetHeight;
+        var aboutBottom = document.getElementById('section-about').offsetHeight - 10 + headerBottom;
+        var educationBottom = document.getElementById('section-education').offsetHeight + aboutBottom;
 
         if (headerTop <= scope.scrollPosition && scope.scrollPosition < headerBottom) {
           scope.isActive = 1;
         } else if (headerBottom <= scope.scrollPosition && scope.scrollPosition < aboutBottom) {
           scope.isActive = 2;
+        } else if (aboutBottom <= scope.scrollPosition && scope.scrollPosition < educationBottom ) {
+          scope.isActive = 3;
         }
 
         scope.scrollPosition = this.pageYOffset;
