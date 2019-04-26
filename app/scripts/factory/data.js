1590f0ee7ed406c2b87d351ade6f127092c9879d
Gobind Ball <bgobind@amazon.com>
2019-04-25 19:06:39 -0700

    update https request call to google spreadsheet

--- app/scripts/factory/data.js
+++ app/scripts/factory/data.js
@@ -5,8 +5,8 @@ angular.module('gballgithubioApp')
   .factory('dataFactory', ['$http', function ($http) {
     var getAboutMe = function (cb) {
       $http.get('https://spreadsheets.google.com/feeds/list/1Shh-V6ypqU7hFQCJxsg9CN5rnWN0d0t6VwviRiHCoe8/1/public/values?alt=json')
-        .success(function (response) {
-          var content = response.feed.entry[0];
+      .then(function (response) {
+        var content = response.data.feed.entry[0];
           response = {
             aboutMe: {
               name: content.gsx$name.$t,
@@ -17,20 +17,21 @@ angular.module('gballgithubioApp')
           };
 
           cb(undefined, response);
-        })
-        .error(function (err) {
-          cb(err);
-        });
+      })
+      .catch(function(err) {
+        console.log(err);
+        cb(err);
+      });
     };
 
     var getEducation = function (cb) {
       $http.get('https://spreadsheets.google.com/feeds/list/1Shh-V6ypqU7hFQCJxsg9CN5rnWN0d0t6VwviRiHCoe8/2/public/values?alt=json')
-        .success(function (response) {
-          var length = response.feed.entry.length;
+        .then(function (response) {
+          var length = response.data.feed.entry.length;
           var tmpResponse = [];
 
           for (var i = 0; i < length; i++) {
-            var content = response.feed.entry[i];
+            var content = response.data.feed.entry[i];
             var obj = {
               schoolName: content.gsx$schoolname.$t,
               location: content.gsx$location.$t,
@@ -49,50 +50,52 @@ angular.module('gballgithubioApp')
 
           cb(undefined, response);
         })
-        .error(function (err) {
+        .catch(function(err) {
+          console.log(err);
           cb(err);
-        });
+        }); 
     };
 
     var getExperience = function (cb) {
       $http.get('https://spreadsheets.google.com/feeds/list/1Shh-V6ypqU7hFQCJxsg9CN5rnWN0d0t6VwviRiHCoe8/3/public/values?alt=json')
-        .success(function (response) {
-          var length = response.feed.entry.length;
-          var tmpResponse = [];
+      .then(function (response) {
+        var length = response.data.feed.entry.length;
+        var tmpResponse = [];
 
-          for (var i = 0; i < length; i++) {
-            var content = response.feed.entry[i];
-            var obj = {
-              company: content.gsx$company.$t,
-              location: content.gsx$location.$t,
-              position: content.gsx$position.$t,
-              info: content.gsx$info.$t,
-              date: content.gsx$date.$t,
-              imageLink: content.gsx$imagelink.$t
-            };
-
-            tmpResponse.push(obj);
-          }
-
-          response = {
-            experience: tmpResponse
+        for (var i = 0; i < length; i++) {
+          var content = response.data.feed.entry[i];
+          var obj = {
+            company: content.gsx$company.$t,
+            location: content.gsx$location.$t,
+            position: content.gsx$position.$t,
+            info: content.gsx$info.$t,
+            date: content.gsx$date.$t,
+            imageLink: content.gsx$imagelink.$t
           };
 
-          cb(undefined, response);
-        })
-        .error(function (err) {
-          cb(err);
-        });
+          tmpResponse.push(obj);
+        }
+
+        response = {
+          experience: tmpResponse
+        };
+
+        cb(undefined, response);
+      })
+      .catch(function(err) {
+        console.log(err);
+        cb(err);
+      });
     };
 
     var getSkills = function (cb) {
       $http.get('https://spreadsheets.google.com/feeds/list/1Shh-V6ypqU7hFQCJxsg9CN5rnWN0d0t6VwviRiHCoe8/4/public/values?alt=json')
-        .success(function (response) {
-          var length = response.feed.entry.length;
+        .then(function (response) {
+          var length = response.data.feed.entry.length;
           var tmpResponse = [];
 
           for (var i = 0; i < length; i++) {
-            var content = response.feed.entry[i];
+            var content = response.data.feed.entry[i];
             var obj = {
               name: content.gsx$name.$t,
               strength: Number(content.gsx$strength.$t)
@@ -107,19 +110,20 @@ angular.module('gballgithubioApp')
 
           cb(undefined, response);
         })
-        .error(function (err) {
+        .catch(function(err) {
+          console.log(err);
           cb(err);
         });
     };
 
     var getPassion = function (cb) {
       $http.get('https://spreadsheets.google.com/feeds/list/1Shh-V6ypqU7hFQCJxsg9CN5rnWN0d0t6VwviRiHCoe8/5/public/values?alt=json')
-        .success(function (response) {
-          var length = response.feed.entry.length;
+        .then(function (response) {
+          var length = response.data.feed.entry.length;
           var tmpResponse = [];
 
           for (var i = 0; i < length; i++) {
-            var content = response.feed.entry[i];
+            var content = response.data.feed.entry[i];
             var obj = {
               type: content.gsx$type.$t,
               info: content.gsx$info.$t,
@@ -135,19 +139,20 @@ angular.module('gballgithubioApp')
 
           cb(undefined, response);
         })
-        .error(function (err) {
+        .catch(function(err) {
+          console.log(err);
           cb(err);
         });
     };
 
     var getQuotes = function (cb) {
       $http.get('https://spreadsheets.google.com/feeds/list/1Shh-V6ypqU7hFQCJxsg9CN5rnWN0d0t6VwviRiHCoe8/6/public/values?alt=json')
-        .success(function (response) {
-          var length = response.feed.entry.length;
+        .then(function (response) {
+          var length = response.data.feed.entry.length;
           var tmpResponse = [];
 
           for (var i = 0; i < length; i++) {
-            var content = response.feed.entry[i];
+            var content = response.data.feed.entry[i];
             var obj = {
               section: content.gsx$section.$t,
               quote: content.gsx$quote.$t
@@ -162,7 +167,8 @@ angular.module('gballgithubioApp')
 
           cb(undefined, response);
         })
-        .error(function (err) {
+        .catch(function(err) {
+          console.log(err);
           cb(err);
         });
     };
